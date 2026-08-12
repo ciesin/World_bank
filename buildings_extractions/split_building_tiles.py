3b1972c5e6e76eccf256cfed9a9e306987b2e28c
Matthew Walter <mswalter@udel.edu>
2026-08-12 16:45:25 -0400

    Add script to split building tiles into smaller raster images
    This script splits raster images into smaller tiles of specified sizes and creates a target folder for the output.
    
    Signed-off-by: Matthew Walter <mswalter@udel.edu>

---
+++ buildings_extractions/split_building_tiles.py
@@ -0,0 +1,48 @@
+import arcpy
+import os
+from pathlib import Path
+
+def split_building_tiles(folder,img,size):
+
+    # Create folder 
+    img_name = Path(img).stem
+    target_folder = (rf"{folder}/{img_name}_{size}")
+    print(target_folder)
+
+    if not arcpy.Exists(target_folder):
+
+        parent_dir = os.path.dirname(target_folder)
+        folder_name = os.path.basename(target_folder)
+
+        arcpy.management.CreateFolder(parent_dir, folder_name)
+        print(f"Folder created: {target_folder}")
+    else:
+        print("Folder already exists.")
+
+    arcpy.management.SplitRaster(
+        in_raster=img,
+        out_folder=target_folder,
+        out_base_name=f"{img_name}_{size}",
+        split_method="SIZE_OF_TILE",
+        format="TIFF",
+        resampling_type="NEAREST",
+        num_rasters="1 1",
+        tile_size=f"{size} {size}",
+        overlap=0,
+        units="PIXELS",
+        cell_size=None,
+        origin=None,
+        split_polygon_feature_class=None,
+        clip_type="NONE",
+        template_extent="DEFAULT",
+        nodata_value=""
+    )
+
+    print(f"raster split for {img}")
+
+#folder = folder path for exporting - subfolder will be created
+#img = path for raster image
+
+split_building_tiles(folder,img,256)
+split_building_tiles(folder,img,512)
+split_building_tiles(folder,img,1024)
